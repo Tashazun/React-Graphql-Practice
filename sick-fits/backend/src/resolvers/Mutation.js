74a04d06ba22a62a502bb09ee7d26c484e8de07b
Tasha Zuniga <tasha@basalt.io>
2019-06-06 14:26:59 -0700

    adding apollo client

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, cxt, info) {
+        
+        const item = await cxt.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = Mutations;
